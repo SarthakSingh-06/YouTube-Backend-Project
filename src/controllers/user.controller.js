8c5787e4b2ea0acf567cfb3a699d4d0aa0df8186
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-08-05 22:32:44 +0530

    add functionality to fetch user channel profile with subscriber counts and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -283,3 +283,76 @@ export const updateUserCoverImage = asyncHandler(async (req, res) => {
             new API_Response(200, existingUser, "Cover image updated successfully")
         );
 });
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if (!username?.trim())
+        throw new API_Error(400, "Username is missing");
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "Subscription",
+                localField: "$_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "Subscription",
+                localField: "$_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                subscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            // check if req.user._id is present as a subscriber to channel
+                            $in: [req.user?._id, "$subscribers.subscriber"],
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                subscribersCount: 1,
+                subscribedToCount: 1,
+                isSubscribed: 1,
+                coverImage: 1,
+                profileImage: 1,
+            }
+        }
+    ]);
+
+    console.log("CHANNEL:", channel);
+
+    if (!channel?.length)
+        throw new API_Error(404, "Channel does not exist");
+
+    return res
+        .status(200)
+        .json(
+            new API_Response(200, channel, "User channel fetched successfully")
+        );
+});
