b9d271ee6acfdef202a12ba3afcc7a544543ed50
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-08-14 16:32:29 +0530

    reduce request body size limit to 16kb for JSON and URL-encoded data

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json({ limit: "128kb" }));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
-app.use(express.urlencoded({ limit: "128kb", extended: true }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(cookieParser());
 
 // configure CORS requests
