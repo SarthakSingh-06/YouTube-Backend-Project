fbff9cf4c59227685e7af78338f745e923e4cf64
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-08-14 17:00:12 +0530

    fix: correct import statement for mongoose model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,4 @@
-import { Schema, models } from "mongoose";
+import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
@@ -41,4 +41,4 @@ const videoSchema = new Schema({
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
-export const Video = models("Video", videoSchema);
+export const Video = model("Video", videoSchema);
