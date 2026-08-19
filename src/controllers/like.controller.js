9c148093d90c9cb5a43f51cd7b67b2cb08a141a7
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-08-20 00:45:19 +0530

    setup likes controller file and implement video like toggling functionality with response handling

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,52 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { API_Response } from "../utils/api-response.js";
+import { API_Error } from "../utils/api-error.js";
+import { Likes } from "../models/likes.model.js";
+import { Comments } from "../models/comments.model.js";
+import { Types as mongooseTypes } from "mongoose";
+
+export const getLikedVideos = asyncHandler(async (req, res) => {
+    // TODO: get all liked videos
+});
+
+export const toggleCommentLike = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+});
+
+export const toggleVideoLike = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    if (!videoId)
+        throw new API_Error(400, "Must provide the video id to toggle like");
+
+    // remove the like if video is already liked
+    const removeLike = await Likes.findOneAndDelete({
+        video: new mongooseTypes.ObjectId(videoId),
+        likedBy: new mongooseTypes.ObjectId(req.user?._id),
+    });
+
+    if (!removeLike) {
+        // like the video if video does not exist
+        const newLike = await Likes.insertOne({
+            video: new mongooseTypes.ObjectId(videoId),
+            likedBy: new mongooseTypes.ObjectId(req.user?._id),
+        });
+
+        // send video liked response
+        return res
+            .status(201)
+            .json(
+                new API_Response(201, newLike, "video liked")
+            );
+    }
+
+    // send video like removed response
+    return res
+        .status(200)
+        .json(
+            new API_Response(200, removeLike, "video liked removed")
+        );
+});
+
+export const togglechannelPostLike = asyncHandler(async (req, res) => {
+    const { channelPostId } = req.params;
+});
