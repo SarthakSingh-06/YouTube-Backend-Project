c64663e65a0b4d27aa298bb59020190d2bc7f5d2
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-08-22 19:01:11 +0530

    feat: implement toggle subscription functionality with validation and response handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,7 +7,34 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params;
-    // TODO: toggle subscription
+    if (!isValidObjectId(channelId))
+        throw new API_Error(400, `${channelId} is not a valid mongodb id`);
+
+    const existingSubscription = await Subscription.findOneAndDelete({
+        channel: new mongooseTypes.ObjectId(channelId),
+        subscriber: new mongooseTypes.ObjectId(req.user?._id)
+    }).select("_id");
+    if (!existingSubscription) {
+        // subscribe the channel if not subscribed
+        const newSubscription = await Subscription.insertOne({
+            channel: new mongooseTypes.ObjectId(channelId),
+            subscriber: new mongooseTypes.ObjectId(req.user?._id)
+        });
+
+        // return channel subcribed response
+        return res
+            .status(200)
+            .json(
+                new API_Response(200, newSubscription, "Subscribed")
+            );
+    }
+
+    // return channel unsubcribed response
+    return res
+        .status(200)
+        .json(
+            new API_Response(200, existingSubscription, "Unsubscribed")
+        );
 });
 
 // controller to return subscriber list of a channel
